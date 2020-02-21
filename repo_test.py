720b3d0ac7a6547be7817031e7ef6c70139654cf
maramalr <malrshooud@gmail.com>
2020-02-20 21:49:04 -0500

    Update repo_test.py

--- repo_test.py
+++ repo_test.py
@@ -11,7 +11,7 @@ class test_get_repo(unittest.TestCase):
         expected = [['Repo: 567_Testing Number of commits: 2 '],['Repo: Animal-Card Number of commits: 2 '], 
                     ['Repo: Assignment0 Number of commits: 1 '], ['Repo: frontend-nanodegree-arcade-game Number of commits: 30 '],
                     ['Repo: frontend-nanodegree-feedreader Number of commits: 26 '], ['Repo: frontend-nanodegree-mobile-portfolio Number of commits: 30 '],
-                    ['Repo: frontend-nanodegree-resume Number of commits: 30 '], ['Repo: gedcom-analyzer Number of commits: 9 '], ['Repo: Github_API Number of commits: 9 '],
+                    ['Repo: frontend-nanodegree-resume Number of commits: 30 '], ['Repo: gedcom-analyzer Number of commits: 9 '], ['Repo: Github_API Number of commits: 11 '],
                     ['Repo: markup Number of commits: 7 '], ['Repo: Neighbourhood-project Number of commits: 21 '],
                     ['Repo: PlanPackRepeat Number of commits: 30 '], ['Repo: Portfolio- Number of commits: 2 '], 
                     ['Repo: SWW810Homeworks Number of commits: 4 '], ['Repo: Triangle567 Number of commits: 11 ']]
