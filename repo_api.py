0837594b73573e0262fa2e8d3879e2a4e21d6376
maramalr <malrshooud@gmail.com>
2020-02-20 20:03:20 -0500

    Commit

---
+++ repo_api.py
@@ -0,0 +1,42 @@
+import json
+import requests
+
+
+def get_repo(github_id):
+
+    user_url = requests.get('https://api.github.com/users/{}/repos'.format(github_id)) #get access to the github ID
+    get_user = json.loads(user_url.text)
+    repo_commit_info = dict()
+
+# check if the user ID is valid 
+    try:
+        get_user[0]['name']
+    except:
+        return 'No user found'
+
+# iterate through each repository    
+    for repos in get_user:
+        repo_name = repos['name']
+        repo_url = requests.get('https://api.github.com/repos/{}/{}/commits'.format(github_id, repo_name)) #access each repository
+        get_repo_info = json.loads(repo_url.text)
+
+        if repo_name in repo_url.text:
+            num = len(get_repo_info)
+        else:
+            num = 0 
+
+        repo_commit_info[repo_name] = num
+    
+    return [['Repo: {} Number of commits: {} '.format(k, v)] for k, v in repo_commit_info.items()]
+
+def main():
+    github_id = input('Please enter your github ID :' )
+    print(get_repo(github_id))
+    
+main()
+
+
+
+
+
+
---
+++ repo_test.py
@@ -0,0 +1,26 @@
+import unittest
+from repo_api import get_repo 
+import json
+import requests
+
+
+class test_get_repo(unittest.TestCase):
+    
+    def test_get_repo(self):
+
+        expected = [['Repo: 567_Testing Number of commits: 2 '],['Repo: Animal-Card Number of commits: 2 '], 
+                    ['Repo: Assignment0 Number of commits: 1 '], ['Repo: frontend-nanodegree-arcade-game Number of commits: 30 '],
+                    ['Repo: frontend-nanodegree-feedreader Number of commits: 26 '], ['Repo: frontend-nanodegree-mobile-portfolio Number of commits: 30 '],
+                    ['Repo: frontend-nanodegree-resume Number of commits: 30 '], ['Repo: gedcom-analyzer Number of commits: 9 '],
+                    ['Repo: markup Number of commits: 7 '], ['Repo: Neighbourhood-project Number of commits: 21 '],
+                    ['Repo: PlanPackRepeat Number of commits: 30 '], ['Repo: Portfolio- Number of commits: 2 '], 
+                    ['Repo: SWW810Homeworks Number of commits: 4 '], ['Repo: Triangle567 Number of commits: 11 ']]
+                    
+        github_id = 'maramalr'
+
+        self.assertEqual(get_repo(github_id), expected)
+
+        
+if __name__ == '__main__':
+    print('Running unit tests')
+    unittest.main()
